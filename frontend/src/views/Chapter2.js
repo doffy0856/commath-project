3829e6fa0627df4570179adae81eda71f0e4908f
larm <larm@email.com>
2020-10-27 20:24:15 +0700

    add

--- frontend/src/views/Chapter2.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Chapter2() {
-    return (
-        <h1>Hello World!</h1>
-    )
-}
--- frontend/src/views/Chapter4.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Chapter4() {
-    return (
-        <h1>Hello World!</h1>
-    )
-}
